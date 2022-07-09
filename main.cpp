326d8aa37f30e9f0060e9973bb87e36bc1483626
f0rodo <kthakorefi@gmail.com>
2022-07-09 18:29:56 +0000

    It is not finding int so using rawnubmer handler

--- main.cpp
+++ main.cpp
@@ -12,6 +12,38 @@ struct MyHandler : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, MyHand
         std::cout << "Key(" << str << ", " << length << ", " << std::boolalpha << copy << ") at " << offset << std::endl;
         return true;
     }
+    bool Bool(bool value) {
+        std::cout << "Bool(" << value << ")" << std::endl;
+        return true;
+    }
+    bool UInt(unsigned int value) {
+        std::cout << "UInt(" << value << ")" << std::endl;
+        return true;
+    }
+    bool UInt64(uint64_t value) {
+        std::cout << "UInt64(" << value << ")" << std::endl;
+        return true;
+    }
+    bool Int(int value) {
+        std::cout << "Int(" << value << ")" << std::endl;
+        return true;
+    }
+    bool Int64(int64_t value) {
+        std::cout << "Int64(" << value << ")" << std::endl;
+        return true;
+    }
+    bool Double(double value ) {
+        std::cout << "Double(" << value << ")" << std::endl;
+        return true;
+    }
+    bool 	RawNumber(const Ch *str, rapidjson::SizeType length, bool copy) {
+        std::cout << "Number(" << str << ", " << length << ", " << std::boolalpha << copy << ")" << std::endl;
+        return true;
+    }
+      bool 	String (const Ch *str, rapidjson::SizeType length, bool copy) {
+        std::cout << "String(" << str << ", " << length << ", " << std::boolalpha << copy << ")" << std::endl;
+        return true;
+    }
     bool EndObject(size_t offset, rapidjson::SizeType memberCount) { std::cout << "EndObject(" << memberCount << ") at " << offset << std::endl; return true; }
     bool StartArray(rapidjson::SizeType offset) { std::cout << "StartArray() at " << offset << std::endl; return true; }
     bool EndArray(rapidjson::SizeType offset, rapidjson::SizeType elementCount) { std::cout << "EndArray(" << elementCount << ") at " << offset << std::endl; return true; }
@@ -22,6 +54,6 @@ int main(int argc, char *argv[])
     rapidjson::IStreamWrapper isw(std::cin);
     MyHandler handler;
     rapidjson::Reader reader;
-    reader.Parse<rapidjson::kParseIterativeFlag>(isw, handler);
+    reader.Parse<rapidjson::kParseIterativeFlag | rapidjson::kParseNumbersAsStringsFlag>(isw, handler);
     return 0;
 }
